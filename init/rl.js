d1de1986b33731b53685ea136dbb1f2eaf057305
Moogle <lizardscruff@users.noreply.github.com>
2018-11-09 17:53:04 +0900

    Add files via upload

--- init/rl.js
+++ init/rl.js
@@ -6,7 +6,7 @@ var ROWS = 10;
 var COLS = 15;
 
 // number of actors per level, including player
-var ACTORS = 10
+var ACTORS = 10;
 
 // the structure of the map
 var map;
@@ -22,7 +22,7 @@ var livingEnemies;
 // points to each actor in its position, for quick searching
 var actorMap;
 
-// intialize phaser, call create() once done
+// initalize phaser, call create() once done
 var game = new Phaser.Game(COLS * FONT * 0.6, ROWS * FONT, Phaser.AUTO, null, {
 	create: create
 });
@@ -34,13 +34,13 @@ function create() {
 	// initialize map
 	initMap();
 
-	// initialize screen
+	// initialize ascii display screen
 	asciidisplay = [];
 	for (var y = 0; y < ROWS; y++) {
 		var newRow = [];
 		asciidisplay.push(newRow);
 		for (var x = 0; x < COLS; x++)
-			newRow.push( initCell('', x, y) );
+			newRow.push(initCell('', x, y));
 	}
 
 	// initialize actors
@@ -53,8 +53,11 @@ function create() {
 
 function initCell(chr, x, y) {
 	// add a single cell in a given position to the ascii display
-	var style = { font: FONT + "px monospace", fill:"#fff"};
-	return game.add.text(FONT*0.6*x, FONT*y, chr, style);
+	var style = {
+		font: FONT + "px monospace",
+		fill:"#fff"
+	};
+	return game.add.text(FONT * 0.6 * x, FONT * y, chr, style);
 }
 
 function initMap() {
@@ -72,10 +75,96 @@ function initMap() {
 	}
 }
 
-function drawMap () {
+function drawMap() {
 	for (var y = 0; y < ROWS; y++)
 		for (var x = 0; x < COLS; x++)
 			asciidisplay[y][x].content = map[y][x];
+}
+
+function randomInt(max) {
+	return Math.floor(Math.random() * max);
+}
+
+function initActors() {
+	// create actors at random locations
+	actorList = [];
+	actorMap = {};
+	for (var e = 0; e < ACTORS; e++) {
+		// create new actor
+		var actor = {
+			x: 0,
+			y: 0, 
+			hp: e == 0 ? 3 : 1
+		};
+		do {
+			// pick a random position that is both a floor and not occupied
+			actor.y = randomInt(ROWS);
+			actor.x = randomInt(COLS);
+		} while (map[actor.y][actor.x] == '#' || actorMap[actor.y + "_" + actor.x] != null);
+		
+		// add references to the actor to the actors list & map
+		actorMap[actor.y + "_" + actor.x] = actor;
+		actorList.push(actor);
+	}
+
+	// the player is the first actor in the list
+	player = actorList[0];
+	livingEnemies = ACTORS - 1;
+}
+
+function drawActors() {
+	for (var a in actorList) {
+		if (actorList[a] != null && actorList[a].hp > 0)
+			asciidisplay[actorList[a].y][actorList[a].x].content = a == 0 ? '' + player.hp : 'e';
+	}
+}
+
+function canGo(actor, dir) {
+	return  actor.x+dir.x >= 0 &&
+			actor.x+dir.x <= COLS - 1 &&
+			actor.y+dir.y >= 0 &&
+			actor.y+dir.y <= ROWS - 1 &&
+			map[actor.y+dir.y][actor.x+dir.x] == '.';
+}
+
+function moveTo(actor, dir) {
+	// check if actor can move in the given direction
+	if (!canGo(actor, dir))
+		return false;
+
+	// moves actor to the new location
+	var newKey = (actor.y + dir.y) +'_'+ (actor.x + dir.x);
+	// if the destination tile has an actor in it
+	if (actorMap[newKey] != null) {
+		// decrement hitpoints of the actor at the destination tile
+		var victim = actorMap[newKey];
+		victim.hp--;
+
+		// if it's dead remove its reference
+		if (victim.hp == 0) {
+			actorMap[newKey] = null;
+			actorList[actorList.indexOf(victim)] = null;
+			if(victim!=player) {
+				livingEnemies--;
+				if (livingEnemies == 0) {
+					// victorymessage
+					var victory = game.add.text(game.world.centerX, game.world.centerY, 'Victory!\nCtrl+r to restart', { fill : '#2e2', align: "center" } );
+					victory.anchor.setTo(0.5, 0.5);
+				}
+			}
+		}
+	} else {
+		// remove reference to the actor's old position
+		actorMap[actor.y + '_' + actor.x] = null;
+
+		// update position
+		actor.y+=dir.y;
+		actor.x+=dir.x;
+
+		// add reference to the actor's new position
+		actorMap[actor.y + '_' + actor.x]=actor;
+	}
+	return true;
 }
 
 function onKeyUp(event) {
@@ -118,87 +207,6 @@ function onKeyUp(event) {
 	drawActors();
 }
 
-function randomInt(max) {
-	return Math.floor(Math.random() * max);
-}
-
-function initActors() {
-	// create actors at random locations
-	actorList = [];
-	actorMap = {};
-	for (var e=0; e<ACTORS; e++) {
-		// create new actor
-		var actor = { x:0, y:0, hp:e == 0?3:1 };
-		do {
-			// pick a random position that is both a floor and not occupied
-			actor.y=randomInt(ROWS);
-			actor.x=randomInt(COLS);
-		} while ( map[actor.y][actor.x] == '#' || actorMap[actor.y + "_" + actor.x] != null );
-		
-		// add references to the actor to the actors list & map
-		actorMap[actor.y + "_" + actor.x]= actor;
-		actorList.push(actor);
-	}
-
-	// the player is the first actor in the list
-	player = actorList[0];
-	livingEnemies = ACTORS-1;
-}
-
-function drawActors() {
-	for (var a in actorList) {
-		if (actorList[a].hp > 0)
-			asciidisplay[actorList[a].y][actorList[a].x].content = a == 0 ? '' + player.hp : 'e';
-	}
-}
-
-function canGo(actor, dir) {
-	return  actor.x+dir.x >= 0 &&
-			actor.x+dir.x <= COLS - 1 &&
-			actor.y+dir.y >= 0 &&
-			actor.y+dir.y <= ROWS - 1 &&
-			map[actor.y+dir.y][actor.x+dir.x] == '.';
-}
-
-function moveTo(actor, dir) {
-	// check if actor can move in the given direction
-	if (!canGo(actor, dir))
-		return false;
-
-	// moves actor to the new location
-	var newKey = (actor.y + dir.y) +'_'+(actor.x + dir.x);
-	// if the destination tile has an actor in it
-	if (actorMap[newKey] != null) {
-		// decrement hitpoints of the actor at the destination tile
-		var victim = actorMap[newKey];
-		victim.hp--;
-
-		// if it's dead remove its reference
-		if (victim.hp == 0) {
-			actorMap[newKey] = null;
-			actorList[actorList.indexOf(victim)] = null;
-			if(victim!=player) {
-				livingEnemies--;
-				if (livingEnemies == 0) {
-					// victorymessage
-					var victory = game.add.text(game.world.centerX, game.centerY, 'Victory!\nCtrl+r to restart', { fill : '#2e2', align: "center" } );
-					victory.anchor.setTo(0.5, 0.5);
-				}
-			}
-		}
-	} else {
-		// remove reference to the actor's old position
-		actorMap[actor.y + '_' + actor.x] = null;
-
-		// update position
-		actor.y+=dir.y;
-		actor.x+=dir.x;
-
-		// add reference to the actor's new position
-		actorMap[actor.y + '_' + actor.x]=actor;
-	}
-	return true;
-}
 
 function aiAct(actor) {
 	var directions = [ { x: -1, y:0 }, { x:1, y:0 }, { x:0, y:-1 }, { x:0, y:1 } ];
@@ -208,7 +216,7 @@ function aiAct(actor) {
 	// if player is far away, walk randomly
 	if (Math.abs(dx) + Math.abs(dy) > 6)
 		// try to walk in random directions until you succeed once
-		while (!moveTo(actor, directions[randomInt(directions.length)]))
+		while (!moveTo(actor, directions[randomInt(directions.length)])) { };
 
 		//otherwise walk towards player
 		if (Math.abs(dx) > Math.abs(dy)) {
